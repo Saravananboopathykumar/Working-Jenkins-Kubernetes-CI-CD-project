8633fc38945c4917769667d57035a5c1a0d51108
saravananboopathykumar <saravananboopathykumar@gmail.com>
2026-01-12 05:36:45 +0000

    recent_commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "CI/CD SUCCESS 🚀 Jenkins + Kubernetes is Working!"
+def hello():
+    return "Flask CI/CD App Running 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
